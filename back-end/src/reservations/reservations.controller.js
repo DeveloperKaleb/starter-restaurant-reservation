3f8d7669c9ac6171a3beed99514805f08071c222
DeveloperKaleb <kalajholt@gmail.com>
2022-02-17 13:20:55 -0600

    Initial creation of 'Create Reservation' page and Dashboard functionality

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,23 @@
+const service = require("./reservations.service");
+
 /**
  * List handler for reservation resources
  */
 async function list(req, res) {
+  const data = await service.list(req)
   res.json({
-    data: [],
+    data,
   });
 }
 
+async function create(req, res) {
+  const data = await service.create(req.body.data)
+  res.json({
+    data
+  })
+}
+
 module.exports = {
+  create,
   list,
 };
---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,24 @@
+const knex = require("../db/connection");
+
+async function list(req) {
+    if (req.query.date) {
+      return knex("reservations")
+        .select("*")
+        .where({"reservation_date": req.query.date})
+    } else {
+      return knex("reservations")
+        .select("*")
+    }
+}
+
+async function create(reservation) {
+    return knex("reservations")
+      .insert(reservation)
+      .returning("*")
+      .then((createdReservation) => createdReservation[0])
+}
+
+module.exports = {
+    create,
+    list,
+}
